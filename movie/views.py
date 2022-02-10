4d69828dd110ce0a81b56e23950b42419d311e21
Paulina Ocampo Duque <usuario@Usuarios-MacBook-Air.local>
2022-02-10 16:16:40 -0500

    commit1

--- movie/views.py
+++ movie/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home (request): 
-    return render (request, 'home.html' , {'name': 'Greg Lim'})
+    return render (request, 'home.html' , {'name': 'Paulina'})
 
 # Create your views here.
